c49d7a421295eeae662779d328de2136707b53ee
Adeniyi Feranmi <you@example.com>
2026-02-20 11:17:04 +0000

    all

--- server.js
+++ server.js
@@ -1,33 +1,35 @@
-import express from "express"
-import cors from "cors"
-const app = express()
-import dotenv from 
-'dotenv'
-dotenv.config()
+import express from "express";
+import cors from "cors";
+const app = express();
+import dotenv from "dotenv";
+dotenv.config();
 
-import connectToDb from "./config/connectToDB.js"
-import bookingRouter from "./router/bookingRouter.js"
-import morgan from "morgan"
+import connectToDb from "./config/connectToDB.js";
+import bookingRouter from "./router/bookingRouter.js";
+import morgan from "morgan";
 
-app.use(express.json())
+app.use(express.json());
 // app.use(cors());
 
-app.use(cors())
-app.use(morgan("dev"))
+app.use(
+  cors({
+    origin: "https://appointment-booking-website-y6m4.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+app.use(morgan("dev"));
 
+const PORT = process.env.PORT;
 
-const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log("App is running✅😎");
+});
 
-app.listen(PORT,() =>{
-    console.log("App is running✅😎");
-    
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to appointment booking website" });
+});
 
-app.get("/",(req,res) =>{
-    res.status(200).json({message:"Welcome to appointment booking website"});
-    
-})
+connectToDb();
 
-connectToDb()
-
-app.use("/booking",bookingRouter)
+app.use("/booking", bookingRouter);
