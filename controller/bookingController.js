8f9d2941acf2fcb851ac72f86cfedd59bca00669
Adeniyi Feranmi <you@example.com>
2026-02-24 13:12:40 +0000

    all

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -57,16 +57,17 @@ export const sendBookingEmail = async (req, res, next) => {
 
     const transporter = nodemailer.createTransport({
       service: "gmail",
-      host: "smtp.gmail.com", 
-      port: 465, 
+      host: "smtp.gmail.com",
+      port: 465,
       secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-      tls: {
-        rejectUnauthorized: false,
-      },
+      connectionTimeout: 10000,
+      greetingTimeout: 10000,
+      socketTimeout: 10000,
+      dnsTimeout: 10000,
     });
     const clientMail = {
       from: `"Limadollz beauty world"<${process.env.EMAIL_USER}>`,
